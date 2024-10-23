4efcfb8f53586872735bf60a3282a562122733c3
CreatorMeow <daniildolia240@gmail.com>
2024-10-23 22:48:57 +0300

    Created Error component

---
+++ src/components/RightBar/Error/Error.tsx
@@ -0,0 +1,8 @@
+import Lottie from 'lottie-react';
+import error from '../../../assets/animations/error.json';
+
+const Error = () => {
+  return <Lottie animationData={error} />;
+};
+
+export default Error;
--- src/components/RightBar/RightBar.tsx
+++ src/components/RightBar/RightBar.tsx
@@ -1,12 +1,14 @@
 import styles from './RightBar.module.css';
 import Sticker from './Sticker/Sticker';
 import Banner from './Banner/Banner';
+import Error from './Error/Error';
 
 const RightBar = () => {
   return (
     <div className={styles.wrapper}>
       <Sticker />
       <Banner />
+      <Error />
     </div>
   );
 };
