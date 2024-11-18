be768f16ae2c0e2feb6ecd7d663d053e262bd573
CreatorMeow <daniildolia240@gmail.com>
2024-11-18 16:53:30 +0300

    Cretated Description component

---
+++ src/components/MainSection/Hero/Description/Description.tsx
@@ -0,0 +1,27 @@
+import styles from './Description.module.css';
+
+const Description = () => {
+  return (
+    <div className={styles.cardBg}>
+      <div className={styles.content}>
+        <h1>
+          FOMO
+          <br />
+          FREDDY
+        </h1>
+        <p>
+          anxious pegeon
+          <br />
+          is building a community-drivven
+          <br />
+          culture where everyone
+          <br />
+          have fun and support each other
+        </p>
+        <button><p>CLICK HERE</p></button>
+      </div>
+    </div>
+  );
+};
+
+export default Description;
--- src/components/MainSection/Hero/Hero.tsx
+++ src/components/MainSection/Hero/Hero.tsx
@@ -1,9 +1,15 @@
 import styles from './Hero.module.css';
+import Description from './Description/Description';
 
 const Hero = () => {
   return (
-      <div className={styles.cardBg}>
+    <div className={styles.container}>
+      <div className={styles.cards}>
+        <Description />
+        <Description />
       </div>
+      <div className={styles.partners}></div>
+    </div>
   );
 };
 
