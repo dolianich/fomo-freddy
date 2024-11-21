c5396a1122f424d970168cebce76b0c47e52446b
CreatorMeow <daniildolia240@gmail.com>
2024-11-22 01:17:39 +0300

    Finished the card with bouncing tits

--- src/components/MainSection/Hero/Card/Card.tsx
+++ src/components/MainSection/Hero/Card/Card.tsx
@@ -1,12 +1,15 @@
 import styles from './Card.module.css';
 import Lottie from 'lottie-react';
 import tits from '../../../../assets/animations/tits.json';
+import banner from '../../../../assets/banner.svg';
 
 const Card = () => {
   return (
     <div className={styles.card}>
       <div className={styles.content}>
-        <div className={styles.text}></div>
+        <div className={styles.text}>
+          <img src={banner} alt="banner" />
+        </div>
         <Lottie animationData={tits} />
       </div>
     </div>
