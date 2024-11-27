de6f531fc39052ae5808409803532b2ff051181c
CreatorMeow <daniildolia240@gmail.com>
2024-11-27 22:28:48 +0300

    Added one responsive breakpoint

--- src/components/LeftBar/Arrow/Arrow.tsx
+++ src/components/LeftBar/Arrow/Arrow.tsx
@@ -1,8 +1,9 @@
 import Lottie from 'lottie-react';
 import arrow from '../../../assets/animations/arrow.json';
+import styles from './Arrow.module.css'
 
 const Arrow = () => {
-  return <Lottie animationData={arrow} />;
+  return <Lottie animationData={arrow} className={styles.arrow}/>;
 };
 
 export default Arrow;
