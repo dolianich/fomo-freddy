f573a0f9edbb31adf618b0e222ff11550ec97702
CreatorMeow <daniildolia240@gmail.com>
2024-12-03 18:22:04 +0300

    Updated Avatar component with switch animation functionality

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,12 +1,25 @@
 import Lottie from 'lottie-react';
 import avatar from '../../assets/animations/avatar.json';
+import scream from '../../assets/animations/scream.json';
 import styles from './Avatar.module.css';
+import { useState } from 'react';
 
 const Avatar = () => {
+  const [state, setState] = useState('idle');
+
+  const animation = () => {
+    setState('scream');
+    setTimeout(() => setState('idle'), 2000);
+  };
+
   return (
-    <div className={styles.container}>
-      <Lottie animationData={avatar} />
-    </div>
+    <button
+      className={styles.container}
+      onClick={() => animation()}
+      disabled={state === 'scream' ? true : false}
+    >
+      <Lottie animationData={state === 'idle' ? avatar : scream} />
+    </button>
   );
 };
 
