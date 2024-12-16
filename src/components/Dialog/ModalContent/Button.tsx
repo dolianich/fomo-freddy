6fae7ebc8bb06e3ec9599d46ed6aeb236427c0fe
CreatorMeow <daniildolia240@gmail.com>
2024-12-16 14:57:26 +0300

    Done with the Dialog for NFT claim

---
+++ src/components/Dialog/ModalContent/Button.tsx
@@ -0,0 +1,11 @@
+import styles from './Button.module.css';
+
+const Button = () => {
+  return (
+    <button className={styles.btn}>
+      <p>CLAIM</p>
+    </button>
+  );
+};
+
+export default Button;
--- src/components/Dialog/ModalContent/FreeNFT.tsx
+++ src/components/Dialog/ModalContent/FreeNFT.tsx
@@ -1,11 +1,15 @@
 import Lottie from 'lottie-react';
 import pill from '../../../assets/animations/PillNFT.json';
 import styles from './FreeNFT.module.css';
+import Button from './Button';
 
 const FreeNFT = () => {
   return (
-    <div className={styles.wrapper}>
-      <Lottie animationData={pill} className={styles.animation}/>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <Lottie animationData={pill} className={styles.animation} />
+      </div>
+      <Button />
     </div>
   );
 };
