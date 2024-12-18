7264a2ae041f322ef452875e413b1054603cfeb1
CreatorMeow <daniildolia240@gmail.com>
2024-12-18 12:14:29 +0300

    Server Routes added

---
+++ server/data/memes.js
@@ -0,0 +1,20 @@
+const memes = [
+    {
+        id: "1",
+        title: 'Meme1'
+    },
+    {
+        id: "2",
+        title: 'Meme2'
+    },
+    {
+        id: "3",
+        title: 'Meme3'
+    },
+    {
+        id: "4",
+        title: 'Meme4'
+    }
+]
+
+module.exports = memes
--- server/server.js
+++ server/server.js
@@ -1,5 +1,19 @@
 const express = require('express')
+const memes = require('./data/memes')
 
 const app = express()
 
-app.listen(6000, console.log('Server is running on port 6000'))
+app.get('/', (req, res) => {
+    res.send('API is running...')
+})
+
+app.get('/api/memes', (req, res) => {
+    res.json(memes)
+})
+
+app.get('/api/memes/:id', (req, res) => {
+    const meme = memes.find((m) => m.id === req.params.id)
+    res.json(meme)
+})
+
+app.listen(4000, console.log('Server is running on port 4000'))
